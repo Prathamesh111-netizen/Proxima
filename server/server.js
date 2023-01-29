6c355236f9954d47211dbda4b07eaf8795ad7bfa
Prathamesh Pawar <79742694+Prathamesh111-netizen@users.noreply.github.com>
2023-01-29 16:50:48 +0530

    stable release

--- server/server.js
+++ server/server.js
@@ -1,43 +1,55 @@
 require("dotenv").config();
-const mongoose = require("mongoose")
-const Document = require("./models/Document.model")
-
+const mongoose = require("mongoose");
+const Document = require("./models/Document.model");
 
 mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
+});
 
 const io = require("socket.io")(process.env.PORT, {
   cors: {
-    origin: process.env.FRONTEND_SERVER ,
+    credentials:true,
+    origin: process.env.FRONTEND_SERVER,
+    transports: ['websocket', 'polling'],
     methods: ["GET", "POST"],
   },
-})
+  allowEIO3: true
+});
 
-io.on("connection", socket => {
-  socket.on("get-document", async documentId => {
-    const document = await findOrCreateDocument(documentId)
-    socket.join(documentId)
-    socket.emit("load-document", document.data)
+io.on("connection", (socket) => {
+  console.log("User Online");
+  socket.on("canvas-data", (data) => {
+    socket.broadcast.emit("canvas-data", data);
+  });
 
-    socket.on("send-changes", delta => {
-      socket.broadcast.to(documentId).emit("receive-changes", delta)
-    })
+  socket.on("get-document", async (documentId) => {
+    const document = await findOrCreateDocument(documentId);
+    socket.join(documentId);
+    socket.emit("load-document", document.data);
 
-    socket.on("save-document", async data => {
-      await Document.findByIdAndUpdate(documentId, { data })
+    socket.on("send-changes", (delta) => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta);
+    });
+
+    socket.on("save-document", async (data) => {
+      await Document.findByIdAndUpdate(documentId, { data });
       // find document by id from astra -> get old fileCID -> delete old file from lighthouse storage
       // upload new file to lighthouse -> save new fileCID to astra
-    })
-  })
-})
+    });
+
+    socket.on("drawing", (data) => {
+      console.log("reached here", data);
+      socket.broadcast.emit("drawing", data);
+    });
+  });
+});
 
 async function findOrCreateDocument(id) {
-  if (id == null) return
+  if (id == null) return;
 
-  const document = await Document.findById(id)
-  if (document) return document
-  return await Document.create({ _id: id, data: "" })
+  const document = await Document.findById(id);
+  if (document) return document;
+  return await Document.create({ _id: id, data: "" });
 }
