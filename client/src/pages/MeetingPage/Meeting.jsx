bee384aede904d2fc6e42c70e06b4d95fa59be1f
Prathamesh Pawar <79742694+Prathamesh111-netizen@users.noreply.github.com>
2023-05-04 13:25:58 +0530

    test

--- client/src/pages/MeetingPage/Meeting.jsx
+++ client/src/pages/MeetingPage/Meeting.jsx
@@ -14,8 +14,10 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Unstable_Grid2";
-
+import { useHuddle01 } from "@huddle01/react";
 import Navbar from "../../components/Navbar";
+import { useLobby, useAudio, useVideo, usePeers  } from "@huddle01/react/hooks";
+import { Video, Audio } from '@huddle01/react/components'
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -24,8 +26,11 @@ const Item = styled(Paper)(({ theme }) => ({
   textAlign: "center",
   color: theme.palette.text.secondary,
 }));
-
 const Meeting = () => {
+  const { initialize, isInitialized } = useHuddle01();
+  const { joinLobby } = useLobby();
+  const { fetchAudioStream, stopAudioStream, error: micError } = useAudio();
+  const { fetchVideoStream, stopVideoStream, error: camError } = useVideo();
   const { id: meetingId } = useParams();
   const [loading, setLoading] = useState(true);
   const huddleClient = getHuddleClient(import.meta.env.VITE_HUDDLE_KEY);
@@ -33,6 +38,7 @@ const Meeting = () => {
   const peersKeys = useHuddleStore((state) => Object.keys(state.peers));
   const [isLoggedin, setIsLoggedin] = useState(false);
 
+  // const { initialize, isInitialized } = useHuddle01();
   useEffect(() => {
     setTimeout(async () => {
       await huddleClient.join(meetingId, {
@@ -40,15 +46,38 @@ const Meeting = () => {
         wallet: "",
         ens: "axit.eth",
       });
+      joinLobby(1);
       console.log("ROOM-STATE", roomState);
       setLoading(false);
     }, 3000);
   }, []);
 
+  const { peerIds } = usePeers();
+
+  useEffect(() => {
+    // its preferable to use env vars to store projectId
+    initialize("KL1r3E1yHfcrRbXsT4mcE-3mK60Yc3YR");
+  }, []);
   return (
     <>
       <div className="MeetingPage">
         <div className="MeetingPageFullPage">
+          {/* Mic */}
+          <button
+            disabled={!fetchAudioStream.isCallable}
+            onClick={fetchAudioStream}
+          >
+            FETCH_AUDIO_STREAM
+          </button>
+
+          {/* Webcam */}
+          <button
+            disabled={!fetchVideoStream.isCallable}
+            onClick={fetchVideoStream}
+          >
+            FETCH_VIDEO_STREAM
+          </button>
+
           {/* {loading && <LoadingScreen />} */}
           {!loading && (
             <div className="MeetingPageComponent">
@@ -77,11 +106,28 @@ const Meeting = () => {
                             />
                           ))}
                         </div>
+                        <div className="grid grid-cols-4">
+                          {peerIds.map((peerId) => (
+                            <Video
+                              key={peer.peerId}
+                              peerId={peer.peerId}
+                              debug
+                            />
+                          ))}
+
+                          {peerIds.map((peerId) => (
+                            <Audio
+                              key={peer.peerId}
+                              peerId={peer.peerId}
+                              debug
+                            />
+                          ))}
+                        </div>
                       </Item>
                     </Grid>
                   </Grid>
                 </Box>
-                <Toolbuttons meetingId={meetingId}/>
+                <Toolbuttons meetingId={meetingId} />
               </div>
             </div>
           )}
